bfbeb61952f9e5128f152889f5c52c5ccfaa91e4
0ov2 <liamdrop@gmail.com>
2022-07-17 14:33:14 +0100

    minor clean up

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const { Client } = require('discord.js');
 require('dotenv').config();
 
 //  :code:
-const { sendMessageToChannel, getDiscordChannelObject } = require("./js/helpers/channelHelpers.js")
+const { sendMessageToChannel } = require("./js/helpers/channelHelpers.js")
 const { createMonkeCommandsbutton, createMoveOctaneButton } = require("./js/buttons/monke-commands");
 const { handleSlowModeSelectMenuInteration, handleClearSlowModeInteraction, moveOctaneInteraction } = require('./js/buttons/handler/button-interactions-handler.js');
 const { setUpAvailabilityCronJobs } = require('./js/cron-jobs/cronJobs.js');
 const { avavilabilityReactionsHandler } = require('./js/helpers/messageHelpers.js');
 
 //  :statics:
-const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"], intents: ["GUILD_VOICE_STATES", "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"]});
+const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"], intents: ["GUILD_VOICE_STATES", "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
 
 
 
@@ -34,7 +34,7 @@ const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"]
     //  Set up button commands in monke-commands channel
     createMonkeCommandsbutton(client)
     createMoveOctaneButton(client)
-    
+
     //  Initialise CRON jobs
     setUpAvailabilityCronJobs(client)
 
@@ -46,33 +46,32 @@ const client = new Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"]
   client.on('interactionCreate', interaction => {
 
     //  Slow mode interactions
-    if (interaction.isSelectMenu() && interaction.customId === "slow-mode"){
+    if (interaction.isSelectMenu() && interaction.customId === "slow-mode") {
       handleSlowModeSelectMenuInteration(client, interaction)
     }
 
-    if (interaction.isButton() && interaction.customId === "clear-slow-mode"){
+    if (interaction.isButton() && interaction.customId === "clear-slow-mode") {
       handleClearSlowModeInteraction(client, interaction)
     }
-    
-    if (interaction.isButton() && interaction.customId === "move-octane"){
+
+    if (interaction.isButton() && interaction.customId === "move-octane") {
       moveOctaneInteraction(client, interaction)
     }
 
   })
 
+  //  handle message reactions
+  client.on("messageReactionAdd", async (reaction, user) => {
 
-client.on("messageReactionAdd", async (reaction, user) => { 
-  const knownAvailbilityChannels = ["op-availability", "dt-availability", "octane-avilability"]
+    const knownAvailbilityChannels = ["op-availability", "dt-availability", "octane-avilability"]
 
+    //  availability
+    console.log(reaction.message.channel.name);
+    if (knownAvailbilityChannels.includes(reaction.message.channel.name)) {
+      avavilabilityReactionsHandler(reaction, user)
+    }
 
-  //  availability
-  console.log(reaction.message.channel.name);
-  if (knownAvailbilityChannels.includes(reaction.message.channel.name)){
-    avavilabilityReactionsHandler(reaction, user)
-  }
-
-})
-
+  })
 
 })()
 
